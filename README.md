3919ffbf6d934d7e319f3b4dfd50e8f9f7d7fe79
hspil <44721499+hspil@users.noreply.github.com>
2020-10-10 13:26:26 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# DeepDreamBot
+
+A Discord bot that runs images through DeepDream
+
+## Dependencies
+
+* `discord.py` 		-	Discord API
+* `python-dotenv`	-	Environment variables from .env
+* `requests`		-	HTTP requests
+
---
+++ deepdreambot.py
@@ -0,0 +1,58 @@
+#.env for API keys
+import os
+from dotenv import load_dotenv
+
+#discord
+from discord import Activity, ActivityType, Embed
+from discord.ext import commands
+
+#API calls
+import requests
+
+#get API keys
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+DEEP_DREAM_KEY = os.getenv("DEEP_DREAM_KEY")
+
+#init bot
+bot = commands.Bot(command_prefix='!')
+
+#on startup
+@bot.event
+async def on_ready():
+	#sanity/success check
+	print(f'{bot.user.name} has connected')
+	
+	#set status message
+	await bot.change_presence(activity=Activity(name="Making dogs look more doglike", type=ActivityType.competing))
+
+
+
+@bot.command(name="dream")
+async def dream(ctx):
+	
+	#get past 2 messages
+	messages = await ctx.history(limit=2).flatten()
+	attachments = messages[1].attachments
+	
+	#if no attachments, assume message is embed link to image
+	if not attachments:
+		image_url = messages[1].content
+	#else the message is a file upload; get file url
+	else:
+		image_url = attachments[0].url
+	
+	#send request to deep dream api
+	deep_dream_output = requests.post(
+		"https://api.deepai.org/api/deepdream",
+		data={
+			'image': image_url,
+		},
+		headers={'api-key': DEEP_DREAM_KEY}
+	).json()["output_url"]
+	
+	#print(deep_dream_output)
+	await ctx.send(deep_dream_output)
+
+
+bot.run(DISCORD_TOKEN)
